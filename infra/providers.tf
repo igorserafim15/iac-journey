272117ad08ba574315796dd6bb4760f54241c437
Igor Serafim <igorserafim.contato@gmail.com>
2024-07-14 21:00:26 -0300

    new: destroy

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,3 +1,19 @@
+# terraform {
+#   required_providers {
+#     digitalocean = {
+#       source  = "digitalocean/digitalocean"
+#       version = "~> 2.0"
+#     }
+
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.36.0"
+#     }
+#   }
+
+#   backend "s3" {}
+# }
+
 provider "aws" {
   region = "us-east-1"
 }
